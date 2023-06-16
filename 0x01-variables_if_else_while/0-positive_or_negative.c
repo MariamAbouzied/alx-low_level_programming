5b7d5e2cd5a9ffb55afeaa9c658f62bdb0b51354
MariamAbouzied <engmaryooom@gmail.com>
2023-06-16 16:15:46 -0700

    chmod +x 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0) {
-		printf("%d , is negative\n")
+		printf("n , is negative\n");
 	}
-	   if (n == 0) {
-                printf("%d , is zero\n")
+	else if (n == 0) {
+                printf("n , is zero\n");
         }
-	      if (n > 0) {
-                printf("%d , is positive\n")
+	else if (n > 0) {
+                printf("n , is positive\n");
         }
+	else {
+		printf("Nothing");
+	}
 	return (0);
 }
