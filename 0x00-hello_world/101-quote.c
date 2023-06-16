143dc9f47c7a178107b146a47857370ef6ad60c6
MariamAbouzied <engmaryooom@gmail.com>
2023-06-15 19:46:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
-/**
- * main - print using write fun.
+/*
+ * main - print using write fun
  * Return: 1
 */
 
-int main(void) 
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
-
+int main(void)
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 return (1);
 }
