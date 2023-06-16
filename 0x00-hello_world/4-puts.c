f9326c89e59730e3f3f24d86481d7fcdc00b3907
MariamAbouzied <engmaryooom@gmail.com>
2023-06-15 18:30:45 -0700

    chmod +x 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main - entry point
  * Description: printing with put
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
