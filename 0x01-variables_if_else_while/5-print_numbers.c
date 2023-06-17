a3673101887e1ccad97507229cb6536330a645cb
MariamAbouzied <engmaryooom@gmail.com>
2023-06-16 19:15:17 -0700

    #include <stdio.h>

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print all letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
