9e66c8d0c035abe6a430abfba5e941cd4884db10
MariamAbouzied <engmaryooom@gmail.com>
2023-06-20 19:10:35 -0700

    mmmm

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,12 @@
  *
  * Return: 1 for lowercase 0 for the rest
  *
+ * @c: ASCII code char.
 */
 
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
