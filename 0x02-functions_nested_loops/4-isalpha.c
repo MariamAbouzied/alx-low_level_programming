95a8408df0734115c4fe551e5c2685c4b2ef2a9f
MariamAbouzied <engmaryooom@gmail.com>
2023-06-20 19:19:11 -0700

    mmmm

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns 1 if c is a letter, lowercase or uppercase Returns 0 otherwise
+ *
+ * @c: ASCII char
+ *
+ * Return: 1 if letter 0 if else
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n');
+}
