82f4f7beb53f421ed6e5c562ab277a4973625588
MariamAbouzied <engmaryooom@gmail.com>
2023-06-20 19:33:55 -0700

    mmmm

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,8 @@
  *
  * @n: an integer
  *
- * Retrun: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero
+ * Retrun: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zer
 */
 
@@ -14,17 +15,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
