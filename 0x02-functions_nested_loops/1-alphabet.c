bcb5da2cceee1a5088bd7d5db716adb8c7a815ef
MariamAbouzied <engmaryooom@gmail.com>
2023-06-20 18:20:13 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Enrty point
+ * Description: print abcd.. to z
+ * Return: always 0
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
+
