c56a99384248b45020857130ab35e2d8f562d20d
MariamAbouzied <engmaryooom@gmail.com>
2023-06-22 13:20:46 -0700

    vcvcv

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - check the code.
  *
- * Return: 1 or 0.
+ * Return: 1 or 0
 */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c< 'Z')
+	if (c >= 'A' && c<= 'Z')
 		return (1);
 	else
 		return (0);
