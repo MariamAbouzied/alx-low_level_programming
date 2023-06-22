88cecfdb9937b848d1a4a77e89bf96d239ae0464
MariamAbouzied <engmaryooom@gmail.com>
2023-06-22 12:44:11 -0700

    vcvcv

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: 1 or 0.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c< 'Z')
+		return (0);
+	else
+		return (0);
+}
